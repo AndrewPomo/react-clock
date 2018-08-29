3e1b6669d7b774dc7a5d8575e9251d520f9b2f94
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-08-29 14:07:33 -0700

    begin work on timer inputs

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,18 @@
 /* eslint-disable */
 import React from 'react';
 
-var Timer = ({videos, handleVideoListEntryTitleClick}) => (
-  <div className="timer">
-  </div>
-);
+var Timer = (props) => {
+  return(
+    <div className="timer">
+      {/* <h2>${time}</h2> */}
+      <form onChange={props.handleInput}>
+        <input placeholder="hrs"></input>
+        <input placeholder="mins"></input>
+        <input placeholder="seconds"></input>
+      </form>
+      {/* buttons */}
+    </div>
+  )
+}
 
 export default Timer;
